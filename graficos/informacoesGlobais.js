2a4498a0b968b4f9aa55cf4f445a7c0bfa0bcdf1
pradomanuu <168730277+pradomanuu@users.noreply.github.com>
2024-10-23 10:23:06 -0300

    Update informacoesGlobais.js

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -3,7 +3,14 @@ const url"https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-glob
 async function visualizarInformacoesGlobal() {
   const res = await fetch(url)
   const dados = await res.json()
-  console.log(dados);
+  const pessoasConectadas = (dados.total_pessoas_conectadas /1a9)
+  const pessoasNoMundo = (dados.total_pessoas_mundo /1e9)
+  const hosas = parseInt(dados.tempo_medio)
+  const minutos = Math.round((dados.tempo_medio - horas) *100)
+  const porcentagemConectadas = ((pessoasConectadas / pessoasNoMundo) *100).tofixed(2)
+
+  const paragrafo = document.createlElement('p')
 }
 
 visualizarInformacoesGlobais()
+
