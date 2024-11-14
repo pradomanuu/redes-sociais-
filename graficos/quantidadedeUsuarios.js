e048df4113b640db2321833671ce237cbaefec2c
pradomanuu <168730277+pradomanuu@users.noreply.github.com>
2024-11-14 08:39:31 -0300

    Update quantidadedeUsuarios.js

--- graficos/quantidadedeUsuarios.js
+++ graficos/quantidadedeUsuarios.js
@@ -1,1 +1,24 @@
+async function quandidadedeUsuarioPorRedes() {
+ cont url = 
+ const res = await fetch(url)
+ const dados = await res.json()
+  const nomeDasRedes = object.keys(dados)
+  const quantidadedeUsuarios = object.values(dados)
+
+  const data = [
+    {
+       x: nomeDasRedes,
+      y: quantidadeDeUsuarios,
+      type: "bar"
+      }
+    ]
+
+  const graficos = document.createElement("div")
+  graico.className = "grafico"
+  document.geTElementbyid"(graficos-container).apendChild(graficos)
+  plotly.newPlot(gtaficos,data)
+}
+
+quantidadedeUsuariosPorRede()
+  
 
